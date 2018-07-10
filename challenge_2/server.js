d06bc6b2edb1423a79934d2f6c45b9929c53ad90
Monsoon <nepzsoon@gmail.com>
2018-07-10 11:27:38 -0700

    Implement succesfull connection to the server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,10 @@
+const express = require('express')
+const app = express() 
+const path = require('path'); 
+
+//serve static assests to handle any http request
+app.use(express.static('client'))
 
 
+
+app.listen(3000, () => console.log('Web Server listening on localhost:3000'))
