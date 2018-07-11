95b429be8c9f8605a505f55b62ab545922ea1334
Monsoon <nepzsoon@gmail.com>
2018-07-11 14:47:57 -0700

    Implement database

---
+++ challenge_3/database/db.js
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS userInfo;
+CREATE DATABASE userInfo;
+USE userInfo;
+
+CREATE TABLE users(
+	first_name TEXT (10),
+	last_name TEXT (10),
+	email VARCHAR (20),
+	password VARCHAR (20)
+	);
+
+CREATE TABLE users_address(
+	street_name VARCHAR (20),
+	city_name VARCHAR (20),
+	state_name VARCHAR (20),
+	zip_code INT (10)
+	);
+
+CREATE TABLE users_banking(
+	credit_card INT (20),
+	exp_date DATE (10),
+	cvv_num INT (10),
+	zip_code INT (10)
+	);
