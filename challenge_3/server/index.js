68a7193b5718900be65d58618176fb3be3091303
Monsoon <nepzsoon@gmail.com>
2018-07-11 19:00:35 -0700

    Implement data model functionality

---
+++ challenge_3/server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// app.use('', (req, res, next) => {
+// 	console.log()
+// })
+
+
+app.listen(PORT, () => console.log('Succesfully connected to port 3000!'))
